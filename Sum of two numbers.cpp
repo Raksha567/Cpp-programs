495632fbf1a04445ec25bb378ce909598741e7fe
Raksha567 <rakshasawade2015@gmail.com>
2024-09-23 08:28:21 +0530

    Add files via upload

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2,res;
+	cout<<"Enter num1 and num2"<<endl;
+	cin>>num1>>num2;
+	res=num1+num2;
+	cout<<"Sum of two number is"<<" "<<res;
+}
